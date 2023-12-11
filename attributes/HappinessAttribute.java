9fc470cfa47c38e8d6eb5b932102b60514efb774
Rosa G. Sierra Villanueva <rosagloriasv27@gmail.com>
2023-12-11 15:04:07 -0500

    Update HappinessAttribute.java

--- attributes/HappinessAttribute.java
+++ attributes/HappinessAttribute.java
@@ -1,17 +1,18 @@
-package attributes; 
+package attributes;
 
 import interfaces.Attribute;
 
 public class HappinessAttribute implements Attribute {
-    private int value; 
+    private int value;
 
     public HappinessAttribute() {
-        this.value = 0; 
+        this.value = 0;
     }
 
     @Override
     public void incrementValue() {
         value++;
+        // Add logic here to cap the value at a maximum if needed
     }
 
     @Override
@@ -23,7 +24,16 @@ public class HappinessAttribute implements Attribute {
 
     @Override
     public String getValue(Integer key) {
-        return "Custom logic to get happiness value description";
+        switch (value) {
+            case 0:
+                return "Sad";
+            case 1:
+                return "Happy";
+            case 2:
+                return "Very Happy";
+            default:
+                return "Unknown"; // Or handle other cases as needed
+        }
     }
 
     @Override
